816ff63b82b3f04f1eaae5daf420af798d4071ed
souhee <souha.saada@esprit.tn>
2021-04-24 22:12:54 +0100

    jsf souhe config

--- src/main/java/tn/esprit/kid/KidzoneApplication.java
+++ src/main/java/tn/esprit/kid/KidzoneApplication.java
@@ -1,7 +1,11 @@
 package tn.esprit.kid;
 
+import javax.faces.webapp.FacesServlet;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KidzoneApplication {
@@ -9,5 +13,10 @@ public class KidzoneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KidzoneApplication.class, args);
 	}
+	
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+	FacesServlet servlet = new FacesServlet();
+	return new ServletRegistrationBean(servlet, "*.jsf"); }
 
 }
